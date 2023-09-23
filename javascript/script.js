5c2fc8a4af1d1a033e6656b025550687ca34ce36
ma2178 <ma2178@hw.ac.uk>
2023-09-23 16:10:47 +0100

    added onmouseout event
    I called the event from the wrong element so I fixed that

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,7 @@
 function peakFictionOn() {
 	document.getElementById("kb").innerHTML = "This is peak fiction!";
 }
+
+function peakFictionOff() {
+	document.getElementById("kb").innerHTML = "Kagurabachi";
+}
